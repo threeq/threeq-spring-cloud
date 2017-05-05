16f5a37c96217dcd3558d03f53854291d8f2de54
three <448987046@qq.com>
2017-05-05 13:50:53 +0800

    integration test project

---
+++ ant-network-test-integration/src/main/java/com/threeq/antnetwork/test/IntegrationTestApp.java
@@ -0,0 +1,8 @@
+package com.threeq.antnetwork.test;
+
+/**
+ * @Date 2017/5/5
+ * @User three
+ */
+public class IntegrationTestApp {
+}
---
+++ ant-network-test-integration/src/test/java/com/threeq/antnetwork/test/integration/UserControllerIntegrationTests.java
@@ -0,0 +1,39 @@
+package com.threeq.antnetwork.test.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.threeq.antnetwork.test.IntegrationTestApp;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Date 2017/5/5
+ * @User three
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = IntegrationTestApp.class)
+//@IntegrationTest
+public class UserControllerIntegrationTests {
+
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
+    //Required to Generate JSON content from Java objects
+
+    /**
+     *
+     */
+    public static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testDemo1() {
+        ResponseEntity response
+                = restTemplate.getForEntity("http://www.baidu.com", String.class);
+
+        Assert.assertEquals(response.getStatusCode(),HttpStatus.OK);
+    }
+}
