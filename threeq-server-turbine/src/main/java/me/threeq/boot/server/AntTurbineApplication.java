b16c622b7162854f69d57c0a59d197a98f7d72ac
three <448987046@qq.com>
2018-03-14 19:07:52 +0800

    服务监控

--- threeq-server-turbine/src/main/java/me/threeq/boot/server/AntTurbineApplication.java
+++ threeq-server-turbine/src/main/java/me/threeq/boot/server/AntTurbineApplication.java
@@ -3,6 +3,7 @@ package me.threeq.boot.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 /**
