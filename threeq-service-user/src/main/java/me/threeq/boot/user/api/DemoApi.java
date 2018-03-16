1793b29f66bf6e67b1bdedf6e65a63ce0f1e6f4b
three <448987046@qq.com>
2018-03-16 16:48:10 +0800

    只用 hytrix 的接口才能监控

--- threeq-service-user/src/main/java/me/threeq/boot/user/api/DemoApi.java
+++ threeq-service-user/src/main/java/me/threeq/boot/user/api/DemoApi.java
@@ -1,5 +1,6 @@
 package me.threeq.boot.user.api;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,17 @@ public class DemoApi {
     public DemoService demoService;
 
     @GetMapping("/demo")
+    @HystrixCommand(fallbackMethod = "defaultDemo")
     public String demo() {
         return demoService.hello();
+    }
+
+    /**
+     * Fallback method
+     *
+     * @return
+     */
+    public String defaultDemo() {
+        return "demo 接口出错，默认返回";
     }
 }
